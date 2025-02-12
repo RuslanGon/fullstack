a1aaaf1443dfa7f46a418c7e0dfdbd0c8f0be289
RuslanGon <ruslan441006@gmail.com>
2025-02-12 18:19:36 +0200

    wer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
    <div>
-<h1 className="text-3xl font-bold text-red-500">Home Page</h1>
+<h1 className="text-3xl font-bold text-blue-500">Home Page</h1>
    </div>
   )
 }
