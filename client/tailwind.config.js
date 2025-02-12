c7606ca3bbdc267efdc31623fa3125c3e969fa90
RuslanGon <ruslan441006@gmail.com>
2025-02-12 17:27:38 +0200

    wer

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,16 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    async () => {
-      const lineClamp = await import('@tailwindcss/line-clamp');
-      return lineClamp.default; // Access the default export
+// tailwind.config.js
+module.exports = {
+    content: [
+      './index.html',
+      './src/**/*.{js,jsx,ts,tsx}',
+    ],
+    theme: {
+      extend: {},
     },
-  ],
-};
+    plugins: [],
+  }
+  
