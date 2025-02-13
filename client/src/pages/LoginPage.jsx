f0f52019e6e3217f66457c778a67348771e5cd39
RuslanGon <ruslan441006@gmail.com>
2025-02-13 16:56:17 +0200

    wer

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,8 +1,34 @@
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <form
+      onSubmit={(e) => e.preventDefault()}
+      className="w-1/4 h-60 mx-auto mt-40"
+    >
+      <h1 className="text-lg text-white text-center">Authorization</h1>
+      <label className="text-xs text-gray-400">
+        Username:
+        <input
+          type="text"
+          placeholder="username"
+          className="mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none placeholder:text-gray-700"/>
+      </label>
+      <label className="text-xs text-gray-400">
+        Password:
+        <input
+          type="Password"
+          placeholder="password"
+          className="mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none placeholder:text-gray-700"/>
+      </label>
+      <div className="flex gap-8 justify-center mt-4">
+      <button type="submit" className="flex justify-between items-center text-xs text-white rounded-sm py-2 px-4 cursor-pointer bg-gray-600">Войти</button>
+      <Link to={'/register'} className="flex justify-center items-center text-sx text-white">
+      Нет аккунта ?
+      </Link>
+      </div>
+    </form>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
