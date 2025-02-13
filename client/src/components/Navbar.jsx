3a0f2baf28f36cf36e28bc73e598088c61552c36
RuslanGon <ruslan441006@gmail.com>
2025-02-13 16:07:48 +0200

    wer

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,18 +1,31 @@
-
 const Navbar = () => {
   return (
     <div className="flex py-4 justify-between items-center">
-        <span className="flex justify-center items-center w-6 h-6 bg-gray-600 text-sx text--white rounded-sm">E</span>
-        <ul className="flex gap-8">
-            <li><a href="" className="text-sx text-gray-400 hover:text-white">Main</a></li>
-            <li><a href="" className="text-sx text-gray-400 hover:text-white"> My posts</a></li>
-            <li><a href="" className="text-sx text-gray-400 hover:text-white">Add post</a></li>
-        </ul>
-        <div className="flex justify-center items-center bg-gray-600 text-xs text-white rounded-sm px-4">
-            <button>Login</button>
-        </div>
+      <span className="flex justify-center items-center w-6 h-6 bg-gray-600 text-xs text-white rounded-sm">
+        E
+      </span>
+      <ul className="flex gap-8">
+        <li>
+          <a href="" className="text-xs text-gray-400 hover:text-white">
+            Main
+          </a>
+        </li>
+        <li>
+          <a href="" className="text-xs text-gray-400 hover:text-white">
+            My posts
+          </a>
+        </li>
+        <li>
+          <a href="" className="text-xs text-gray-400 hover:text-white">
+            Add post
+          </a>
+        </li>
+      </ul>
+      <div className="flex justify-center items-center bg-gray-600 text-xs text-white rounded-sm px-4 py-2 cursor-pointer">
+        <button>Login</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
