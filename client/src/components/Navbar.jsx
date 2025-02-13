b2e538922fba25c141140c2197f3bc12747ef49e
RuslanGon <ruslan441006@gmail.com>
2025-02-13 15:50:20 +0200

    wer

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="flex py-4 justify-between items-center">
+        <span className="flex justify-center items-center w-6 h-6 bg-gray-600 text-sx text--white rounded-sm">E</span>
+        <ul className="flex-gap-8">
+            <li><a href="" className="text-sx text-gray-400 hover:text-white">vfev</a></li>
+        </ul>
+    </div>
   )
 }
 
