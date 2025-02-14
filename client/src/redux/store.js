2c476df26e906e39d7c230687eec81d237e3f3f5
RuslanGon <ruslan441006@gmail.com>
2025-02-14 16:57:45 +0200

    wer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { authSlice } from "./features/auth/authSlice.js";
+
 
 export const store = configureStore({
     reducer: {
-        
+        auth: authSlice
     }
 })
