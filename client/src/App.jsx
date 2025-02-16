375a290c4b274376f0d711ecc10b2cb39b7e0d67
RuslanGon <ruslan441006@gmail.com>
2025-02-16 11:30:06 +0200

    wer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout.jsx";
 import MainPage from "./pages/MainPage.jsx";
 import PostsPage from "./pages/PostsPage.jsx";
@@ -7,18 +7,20 @@ import AddPostPage from "./pages/AddPostPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import EditPostPage from "./pages/EditPostPage.jsx";
-import {ToastContainer } from 'react-toastify'
-import { useDispatch } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getMe } from "./redux/features/auth/authSlice.js";
 
 function App() {
-
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const isAuth = useSelector((state) => Boolean(state.auth.token)); // Проверка авторизации
 
   useEffect(() => {
-    dispatch(getMe())
-  },[dispatch])
+    if (isAuth) {
+      dispatch(getMe());
+    }
+  }, [dispatch, isAuth]);
 
   return (
     <Layout>
@@ -28,10 +30,10 @@ function App() {
         <Route path=":id" element={<PostPage />} />
         <Route path=":id/edit" element={<EditPostPage />} />
         <Route path="/new" element={<AddPostPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={isAuth ? <Navigate to="/login" /> : <RegisterPage />} />
+        <Route path="/login" element={isAuth ? <Navigate to="/" /> : <LoginPage />} />
       </Routes>
-      <ToastContainer position='bottom-right' />
+      <ToastContainer position="bottom-right" />
     </Layout>
   );
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,50 +1,63 @@
 import { useDispatch, useSelector } from "react-redux";
-import { Link, NavLink } from "react-router-dom";
-// import { checkIsAuth } from "../redux/features/auth/authSlice.js";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import { logout } from "../redux/features/auth/authSlice";
 
-
 const Navbar = () => {
-
   const dispatch = useDispatch();
-  const isAuth = useSelector(state => Boolean(state.auth.token));
+  const navigate = useNavigate();  // Добавьте useNavigate
+  const isAuth = useSelector((state) => Boolean(state.auth.token));
 
   const handleLogout = () => {
     dispatch(logout());
+    navigate("/register");  // Направление на страницу регистрации после выхода
   };
 
   const activeStyles = {
-    color: 'white'
-  }
+    color: 'white',
+  };
 
   return (
-    <div className='flex py-4 justify-between items-center'>
-    <span className='flex justify-center items-center w-6 h-6 bg-gray-600 text-xs text-white rounded-sm'>
+    <div className="flex py-4 justify-between items-center">
+      <span className="flex justify-center items-center w-6 h-6 bg-gray-600 text-xs text-white rounded-sm">
         E
-    </span>
-     {isAuth &&  <ul className="flex gap-8">
-        <li>
-          <NavLink style={({isActive}) => isActive ? activeStyles : undefined} to={'/'} href="" className="text-xs text-gray-400 hover:text-white">
-            Main
-          </NavLink>
-        </li>
-        <li>
-          <NavLink style={({isActive}) => isActive ? activeStyles : undefined} to={'/posts'} href="" className="text-xs text-gray-400 hover:text-white">
-            My posts
-          </NavLink>
-        </li>
-        <li>
-          <NavLink style={({isActive}) => isActive ? activeStyles : undefined} to={'/new'} href="" className="text-xs text-gray-400 hover:text-white">
-            Add post
-          </NavLink>
-        </li>
-      </ul>}
+      </span>
+      {isAuth && (
+        <ul className="flex gap-8">
+          <li>
+            <NavLink
+              style={({ isActive }) => (isActive ? activeStyles : undefined)}
+              to={"/"}
+              className="text-xs text-gray-400 hover:text-white"
+            >
+              Main
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              style={({ isActive }) => (isActive ? activeStyles : undefined)}
+              to={"/posts"}
+              className="text-xs text-gray-400 hover:text-white"
+            >
+              My posts
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              style={({ isActive }) => (isActive ? activeStyles : undefined)}
+              to={"/new"}
+              className="text-xs text-gray-400 hover:text-white"
+            >
+              Add post
+            </NavLink>
+          </li>
+        </ul>
+      )}
       <div className="flex justify-center items-center bg-gray-600 text-xs text-white rounded-sm px-4 py-2 cursor-pointer">
-       {isAuth ? (
-         <button onClick={handleLogout}>Exit</button>
-       ) : (
-       <Link to={'/login'}>Login</Link>
-       )}
+        {isAuth ? (
+          <button onClick={handleLogout}>Exit</button>
+        ) : (
+          <Link to={"/login"}>Login</Link>
+        )}
       </div>
     </div>
   );
