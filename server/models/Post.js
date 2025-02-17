ed45a90ca237e31d2ec77f2d3c5fd0cb3493b216
RuslanGon <ruslan441006@gmail.com>
2025-02-17 13:18:26 +0200

    wer

--- server/models/Post.js
+++ server/models/Post.js
@@ -21,7 +21,7 @@ const PostSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
-    avtor: {
+    author: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
