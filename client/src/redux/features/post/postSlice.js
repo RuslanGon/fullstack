2bb4952781265de408578682ade8295880cbad8e
RuslanGon <ruslan441006@gmail.com>
2025-02-17 16:31:03 +0200

    wer

---
+++ client/src/redux/features/post/postSlice.js
@@ -0,0 +1,40 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "../../../utils/axios.js";
+
+const initialState = {
+  posts: [],
+  popularPost: [],
+  loading: false
+};
+
+export const createPost = createAsyncThunk(
+  "post/createPost", async (params) => {
+    try {
+      const { data } = await axios.post("/posts", params);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+export const postSlice = createSlice({
+    name: "post",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder 
+        .addCase(createPost.pending, (state) => {
+          state.loading = true
+        })
+        .addCase(createPost.fulfilled, (state, action) => {
+            state.loading = false,
+            state.posts.push(action.payload)
+        })
+        .addCase(createPost.rejected, (state) => {
+            state.loading = false
+        })
+    }
+});
+
+export default postSlice.reducer;
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,11 @@
 
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/authSlice.js"; // Импортируем reducer
+import postReducer from "./features/post/postSlice.js";
 
 export const store = configureStore({
     reducer: {
-        auth: authReducer // Передаём именно reducer
+        auth: authReducer,
+        post: postReducer
     }
 });
