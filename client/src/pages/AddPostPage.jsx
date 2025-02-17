3b8ab5da201f9c49b1b93153410bd074e6e97e97
RuslanGon <ruslan441006@gmail.com>
2025-02-17 15:41:33 +0200

    wer

--- client/src/pages/AddPostPage.jsx
+++ client/src/pages/AddPostPage.jsx
@@ -1,8 +1,16 @@
-
 const AddPostPage = () => {
   return (
-    <div>AddPostPage</div>
-  )
-}
+    <form onSubmit={(e) => e.preventDefault()} className="w-1/3 mx-auto py-10">
+      <label className="text-gray-300 py-2 bg-gray-500 text-xs flex items-center justify-center border-2 border-dotted cursor-pointer">
+        Прикрепить изобраение
+        <input type="file" className="hidden" />
+      </label>
+      <div className="flex object-cover py-2">IMAGE</div>
+      <label className="text-xs text-white opacity-70">Заголовок поста
+      <input type="text" className="mt-1 text-black w-full rounded-lg bg-gray-400" />
+      </label>
+    </form>
+  );
+};
 
-export default AddPostPage
+export default AddPostPage;
