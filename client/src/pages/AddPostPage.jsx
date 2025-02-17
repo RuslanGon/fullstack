20827ccdf3cbd216eb3fae9d4a3e314afbcafa8f
RuslanGon <ruslan441006@gmail.com>
2025-02-17 16:36:31 +0200

    wer

--- client/src/pages/AddPostPage.jsx
+++ client/src/pages/AddPostPage.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 const AddPostPage = () => {
+
+const [tittle, setTitle] = useState('')
+const [text, setText] = useState('')
+const [image, setImage] = useState('')
+
+
   return (
     <form onSubmit={(e) => e.preventDefault()} className="w-1/3 mx-auto py-10">
       <label className="text-gray-300 py-2 bg-gray-500 text-xs flex items-center justify-center border-2 border-dotted cursor-pointer">
