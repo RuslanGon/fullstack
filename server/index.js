455bdea85ab49d97df5f7924e5b4369a7f29893d
RuslanGon <ruslan441006@gmail.com>
2025-02-18 12:34:53 +0200

    wer

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME;
 
+
 // Middleware
 app.use(cors())
 app.use(express.json())
