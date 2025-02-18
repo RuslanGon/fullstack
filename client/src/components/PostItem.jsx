6541ec3bd3ef41907e536cf272a3e5cfd4b803b9
RuslanGon <ruslan441006@gmail.com>
2025-02-18 14:27:11 +0200

    wre

--- client/src/components/PostItem.jsx
+++ client/src/components/PostItem.jsx
@@ -3,7 +3,12 @@ const PostItem = () => {
   return (
     <div className="flex flex-col basis-1/4 flex-grow">
         <div className="">IMAGE</div>
-        <div className="flex justify-between items-center pt-2"></div>
+        <div className="flex justify-between items-center pt-2">
+            <div className="text-xs text-white opacity-50">USERNAME</div>
+            <div className="text-xs text-white opacity-50">DATA</div>
+        </div>
+        <div className="text-xl text-white">POST TITLE</div>
+        <p className="text-xs text-white opacity-60 pt-4">POST TEXT</p>
     </div>
   )
 }
