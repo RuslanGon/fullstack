48ec468fe09aa4fd7be929c3e7ad19eb10f95457
RuslanGon <ruslan441006@gmail.com>
2025-02-21 13:36:58 +0200

    wer

--- client/src/pages/EditPostPage.jsx
+++ client/src/pages/EditPostPage.jsx
@@ -1,6 +1,6 @@
 import { useCallback, useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import axios from '../utils/axios.js'
 import { updatePost } from "../redux/features/post/postSlice.js";
 
@@ -53,6 +53,11 @@ const EditPostPage = () => {
 
   return (
     <form className="w-1/3 mx-auto py-10" onSubmit={(e) => e.preventDefault()}>
+       <button className="flex justify-center items-center bg-gray-600 text-xs text-white rounded-sm py-2 px-4">
+        <Link to={"/posts"} className="flex">
+          Назад
+        </Link>
+      </button>
       <label className="text-gray-300 py-2 bg-gray-600 text-xs mt-2 flex items-center justify-center border-2 border-dotted cursor-pointer">
         Прикрепить изорбажение:
         <input
