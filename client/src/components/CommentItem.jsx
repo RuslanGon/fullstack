b85df9aebdb495732b69c7e57c000de91e258631
RuslanGon <ruslan441006@gmail.com>
2025-02-21 18:08:27 +0200

    wer

--- client/src/components/CommentItem.jsx
+++ client/src/components/CommentItem.jsx
@@ -1,10 +1,15 @@
-const CommentItem = ({ comment }) => {
-  return (
-    <div className="flex items-center gap-3">
-      <div className="flex items-center justify-center shrink-0 rounded-full w-10 h-10 bg-blue-300 text-sm">AVATAR</div>
-      <div className="flex text bg-gray-300 text-{10px}">{comment.comment}</div>
-    </div>
-  );
-};
+
+export const CommentItem = ({ comment }) => {
+    const avatar = comment.comment.trim().toUpperCase().split('').slice(0, 2)
+    return (
+        <div className='flex items-center gap-3'>
+            <div className='flex items-center justify-center shrink-0 rounded-full w-10 h-10 bg-blue-300 text-sm'>
+                {avatar}
+            </div>
+            <div className='flex text-gray-300 text-[10px]'>{comment.comment}</div>
+        </div>
+    )
+}
 
 export default CommentItem;
+
