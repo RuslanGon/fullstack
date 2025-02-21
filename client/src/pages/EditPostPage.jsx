2c48a3e723f3eb15976ca866f0f497d01f981172
RuslanGon <ruslan441006@gmail.com>
2025-02-21 13:37:49 +0200

    wer

--- client/src/pages/EditPostPage.jsx
+++ client/src/pages/EditPostPage.jsx
@@ -102,7 +102,7 @@ const EditPostPage = () => {
           onClick={submitHandler}
           className="flex justify-center items-center bg-gray-600 text-xs text-white rounded-sm py-2 px-4"
         >
-          Сохранить
+          Обновить
         </button>
 
         <button
