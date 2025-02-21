8287f1da3c146f9d4e30c276f8cd82ce4d059338
RuslanGon <ruslan441006@gmail.com>
2025-02-21 19:04:30 +0200

    wer

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
- plugins: [react(), tailwindcss()],
-build: {
-sourcemap: true,
-}
+  plugins: [react(), tailwindcss()],
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      external: ['react-router-dom'], 
+    },
+  },
 });
